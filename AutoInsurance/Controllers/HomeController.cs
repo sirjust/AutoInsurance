5fb150e791f2ff2a09c32ada1b1e435f26a605a1
Justin <theoperaking@yahoo.com>
2018-06-20 12:30:42 -0700

    initial push

---
+++ AutoInsurance/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using AutoInsurance.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutoInsurance.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GetQuote(string FirstName, string LastName, string EmailAddress/*, 
+            DateTime DateOfBirth,
+            int CarYear, string CarMake, string CarModel, bool Dui, int Tickets, string Coverage*/)
+        // these have to match the name attribute of the input form 
+        {
+            if (string.IsNullOrEmpty(FirstName) || string.IsNullOrEmpty(LastName)) /*|| string.IsNullOrEmpty(EmailAddress)
+                || string.IsNullOrEmpty(CarMake) || string.IsNullOrEmpty(CarModel) || string.IsNullOrEmpty(Coverage))*/
+            // i still need to account for the datetime, bool and int
+            {
+                return View("~/Views/Shared/Error.cshtml");
+            }
+            else
+            {
+                using (BEInsuranceEntities db = new BEInsuranceEntities())
+                {
+                    var getQuote = new Customer();
+                    getQuote.FirstName = FirstName;
+                    getQuote.LastName = LastName;
+                    getQuote.EmailAddress = EmailAddress;
+                    /*getQuote.DateOfBirth = DateOfBirth;
+                    getQuote.CarYear = CarYear;
+                    getQuote.CarMake = CarMake;
+                    getQuote.CarModel = CarModel;
+                    getQuote.Dui = Dui;
+                    getQuote.Tickets = Tickets;
+                    getQuote.Coverage = Coverage;*/
+
+                    db.Customers.Add(getQuote);
+                    db.SaveChanges();
+                }
+                return View("Quote");
+            }
+        }
+
+
+        public ActionResult Admin()
+        {
+            using (BEInsuranceEntities db = new BEInsuranceEntities())
+            {
+                var AllCustomers = db.Customers;
+                var CustomerList = new List<Customer>();
+            }
+            
+                return View("Admin");
+        }
+    }
+}
---
+++ AutoInsurance/Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AutoInsurance.Models
+{
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int CarYear { get; set; }
+        public string CarMake { get; set; }
+        public string CarModel { get; set; }
+        public bool Dui { get; set; }
+        public bool Tickets { get; set; }
+        public string Coverage { get; set; }
+    }
+}
